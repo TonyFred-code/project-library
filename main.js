6f1e4773740468c5ed6c36433662f70b9aa8112e
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-10-20 01:55:13 +0100

    remove dummy code and add more usable code and html content

--- main.js
+++ main.js
@@ -1,55 +1,71 @@
-const showButton = document.getElementById('showDialog');
-const favDialog = document.getElementById('favDialog');
-const outputBox = document.querySelector('output');
-const selectEl = favDialog.querySelector('select');
-const confirmBtn = favDialog.querySelector('#confirmBtn');
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+const closeDialogBtn = favDialog.querySelector("#closeBtn");
 const form = document.querySelector("form");
 
 // "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener('click', () => {
-    favDialog.showModal();
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
 });
 
-// "Favorite animal" input sets the value of the submit button
-selectEl.addEventListener('change', (e) => {
-  confirmBtn.value = selectEl.value;
+closeDialogBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  console.log("closed without saving");
+  favDialog.close();
 });
 
 // "Confirm" button triggers "close" on dialog because of [method="dialog"]
-favDialog.addEventListener('close', (e) => {
-  outputBox.value = favDialog.returnValue === 'default' ? "No return value." : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+favDialog.addEventListener("close", (e) => {
+  console.log("closed");
 });
-confirmBtn.addEventListener('click', (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-//   console.log(form.favAnimal.value)
-  console.log(form.title.value);
-  console.log(form.author.value);
-  console.log(form.read.checked);
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  validateForm();
+
+  if (form.title.value && form.author.value && Number(form.pages.value) > 0) {
+    console.log(form.title.value);
+    console.log(form.author.value);
+    console.log(form.read.checked);
     console.log(form.pages.value);
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+    addBookToLibrary(form.title.value, form.author.value, form.pages.value, form.read.checked)
+    favDialog.close();
+  }
 });
 
+function validateForm() {
+  if (form.title.value.trim() === "") {
+    console.log("not valid");
+    form.title.validity.valid = false;
+    form.title.focus();
+  }
+}
 
 // Library Storage ?
 const library = [];
 
-
 // Book constructor
 
 function Book(title, author, pageNo, read) {
-    this.title = title;
-    this.author = author;
-    this.pageNo = pageNo;
-    this.read = read;
+  this.title = title;
+  this.author = author;
+  this.pageNo = pageNo;
+  this.read = read;
 }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-}
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
 function addBookToLibrary(bookTitle, bookAuthor, pageNo, read) {
-    const newBook = new Book(bookTitle, bookAuthor, pageNo, read);
+  const newBook = new Book(bookTitle, bookAuthor, pageNo, read);
 
-    library.push(newBook);
-    console.log(library);
+  library.push(newBook);
+  console.log(library);
+}
+
+function createBookEntryCard() {
+
 }
