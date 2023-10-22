6e593eabf3b47dbaf313762acc4c0f3587c55cca
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-10-22 20:17:55 +0100

    improve time to display card entries
    switch to using two library storage arrays

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const authorErrContainer = document.querySelector(".book-author.error-msg");
 const pagesNoErrContainer = document.querySelector(".book-pages.error-msg");
 const addDummyBookEntriesBtn = document.querySelector(".add-dummy-book-entries");
 
+// Library Storage ?
+let displayedLibrary = [];
+let notDisplayedLibrary = [];
+
 addDummyBookEntriesBtn.addEventListener("click", displayDummyBooks);
 
 // "Show the dialog" button opens the <dialog> modally
@@ -63,7 +67,9 @@ confirmBtn.addEventListener("click", (event) => {
     form.read.checked
   );
 
-  displayBooksInLibrary(library);
+  displayBooksInLibrary(notDisplayedLibrary);
+  displayedLibrary.push(...notDisplayedLibrary);
+  notDisplayedLibrary = [];
   bookEntryDialog.close();
 });
 
@@ -80,8 +86,7 @@ function displayInvalidMessages(errMsg, errDisplayElm) {
   errDisplayElm.classList.remove("hidden");
 }
 
-// Library Storage ?
-const library = [];
+
 
 // Book constructor
 
@@ -90,16 +95,21 @@ function Book(title, author, pageNo, read) {
   this.author = author;
   this.pageNo = pageNo;
   this.read = read;
+  this.displayed = false;
 }
 
 Book.prototype.toggleRead = function () {
   this.read = !this.read;
 };
 
+Book.prototype.markDisplayed = function () {
+  this.displayed = true;
+}
+
 function addBookToLibrary(bookTitle, bookAuthor, pageNo, read) {
   const newBook = new Book(bookTitle, bookAuthor, pageNo, read);
 
-  library.push(newBook);
+  notDisplayedLibrary.push(newBook);
 }
 
 function displayBooksInLibrary(library) {
@@ -111,50 +121,55 @@ function displayBooksInLibrary(library) {
   // else create cards for each entry
   let len = library.length;
 
-  libraryDisplay.textContent = "";
+  // libraryDisplay.textContent = "";
 
   console.time("Book Entries");
   for (let i = 0; i < len; i++) {
-    console.time(`Book Entry ${i}`);
-    const bookEntry = document.createElement("div");
-    bookEntry.setAttribute("class", "book-entry");
-    bookEntry.setAttribute("data-index", `${i}`);
+    if (library[i].displayed) {
+      console.log("displayed");
+    } else {
+      console.time(`Book Entry ${i}`);
+      const bookEntry = document.createElement("div");
+      bookEntry.setAttribute("class", "book-entry");
+      bookEntry.setAttribute("data-index", `${i}`);
 
-    const bookTitle = document.createElement("h2");
-    bookTitle.setAttribute("class", "book-title");
-    bookTitle.textContent = `${library[i].title}`;
+      const bookTitle = document.createElement("h2");
+      bookTitle.setAttribute("class", "book-title");
+      bookTitle.textContent = `${library[i].title}`;
 
-    const bookAuthor = document.createElement("p");
-    bookAuthor.setAttribute("class", "book-author");
-    bookAuthor.textContent = `${library[i].author}`;
+      const bookAuthor = document.createElement("p");
+      bookAuthor.setAttribute("class", "book-author");
+      bookAuthor.textContent = `${library[i].author}`;
 
-    const bookPageNumber = document.createElement("p");
-    bookPageNumber.setAttribute("class", "book-page-numbers");
-    bookPageNumber.textContent = `${library[i].pageNo} ${
-      library[i].pageNo === 1 ? "page" : "pages"
-    }`;
+      const bookPageNumber = document.createElement("p");
+      bookPageNumber.setAttribute("class", "book-page-numbers");
+      bookPageNumber.textContent = `${library[i].pageNo} ${
+        library[i].pageNo === 1 ? "page" : "pages"
+      }`;
 
-    const readBtn = document.createElement("button");
-    readBtn.setAttribute("class", "read");
-    readBtn.setAttribute("data-state", `${library[i].read}`);
-    readBtn.setAttribute("type", "button");
-    readBtn.textContent = `${library[i].read ? "READ" : "NOT READ"}`;
-    readBtn.addEventListener("click", toggleBookEntryReadStatus);
+      const readBtn = document.createElement("button");
+      readBtn.setAttribute("class", "read");
+      readBtn.setAttribute("data-state", `${library[i].read}`);
+      readBtn.setAttribute("type", "button");
+      readBtn.textContent = `${library[i].read ? "READ" : "NOT READ"}`;
+      readBtn.addEventListener("click", toggleBookEntryReadStatus);
 
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove-book-entry");
-    removeBtn.setAttribute("data-index", `${i}`);
-    removeBtn.textContent = "REMOVE";
-    removeBtn.addEventListener("click", removeBookEntry);
+      const removeBtn = document.createElement("button");
+      removeBtn.classList.add("remove-book-entry");
+      removeBtn.setAttribute("data-index", `${i}`);
+      removeBtn.textContent = "REMOVE";
+      removeBtn.addEventListener("click", removeBookEntry);
 
-    bookEntry.appendChild(bookTitle);
-    bookEntry.appendChild(bookAuthor);
-    bookEntry.appendChild(bookPageNumber);
-    bookEntry.appendChild(readBtn);
-    bookEntry.appendChild(removeBtn);
-    libraryDisplay.appendChild(bookEntry);
+      bookEntry.appendChild(bookTitle);
+      bookEntry.appendChild(bookAuthor);
+      bookEntry.appendChild(bookPageNumber);
+      bookEntry.appendChild(readBtn);
+      bookEntry.appendChild(removeBtn);
+      libraryDisplay.appendChild(bookEntry);
+      library[i].displayed = true;
 
-    console.timeEnd(`Book Entry ${i}`);
+      console.timeEnd(`Book Entry ${i}`);
+    }
   }
   console.timeEnd("Book Entries");
 }
@@ -163,28 +178,27 @@ function toggleBookEntryReadStatus() {
   let element = this;
   let parentEntryIndex = this.parentElement.getAttribute("data-index");
 
-  library[parentEntryIndex].toggleRead();
+  displayedLibrary[parentEntryIndex].toggleRead();
   element.textContent = `${
-    library[parentEntryIndex].read ? "READ" : "NOT READ"
+    displayedLibrary[parentEntryIndex].read ? "READ" : "NOT READ"
   }`;
-  element.setAttribute("data-state", `${library[parentEntryIndex].read}`);
+  element.setAttribute("data-state", `${displayedLibrary[parentEntryIndex].read}`);
 }
 
 function removeBookEntry() {
   let parentIndex = this.getAttribute("data-index");
 
-  library.splice(parentIndex, 1);
+  displayedLibrary.splice(parentIndex, 1);
   libraryDisplay.removeChild(this.parentElement);
-  displayBooksInLibrary(library);
 }
 
-displayBooksInLibrary(library);
+displayBooksInLibrary(notDisplayedLibrary);
 
 function displayDummyBooks() {
-  let libraryLen = library.length;
+  let libraryLen = displayedLibrary.length;
   let entriesNo = 0;
 
-  if (library.length === 0) {
+  if (libraryLen === 0) {
     entriesNo = getRndInt(7, 12);
   } else {
     entriesNo = libraryLen + 1;
@@ -196,10 +210,12 @@ function displayDummyBooks() {
     let pagesNo = getRndInt(200, 1000);
     let checked = pagesNo % 2 === 0;
     let book = new Book(title, author, pagesNo, checked);
-    library.push(book);
+    notDisplayedLibrary.push(book);
   }
 
-  displayBooksInLibrary(library);
+  displayBooksInLibrary(notDisplayedLibrary);
+  displayedLibrary.push(...notDisplayedLibrary);
+  notDisplayedLibrary = [];
 }
 
 function getRndInt(min, max) {
